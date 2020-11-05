29fd969629b9e4958d9aba2f208299088e5fa7c5
Ida <67982739+igk1024@users.noreply.github.com>
2020-11-05 10:38:15 -0600

    carousel updates

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,7 +1,27 @@
-// testing the bulma slider extension
+// the bulma slider extension
 
 bulmaCarousel.attach('#carousel-slider', {
   slidesToScroll: 1,
   slidesToShow: 3,
   infinite: true,
+  navigationKeys: true,
+  pagination:false,
 });
+
+
+// Loop on each carousel initialized
+for(var i = 0; i < carousels.length; i++) {
+	// Add listener to  event
+	carousels[i].on('before:show', state => {
+		console.log(state);
+	});
+}
+
+// Access to bulmaCarousel instance of an element
+var element = document.querySelector('.card-image');
+if (element && element.bulmaCarousel) {
+	// bulmaCarousel instance is available as element.bulmaCarousel
+	element.bulmaCarousel.on('before-show', function(state) {
+		console.log(state);
+	});
+}
