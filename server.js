220b340bb983564a1854ad61e22b2851d4d759d5
TZeuch <zeuchtony@gmail.com>
2020-11-05 21:24:13 -0600

    add back in sequelize stuff

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-// const sequelize = require('./config/connection.js');
+const sequelize = require('./config/connection.js');
 const path = require('path');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
@@ -9,24 +9,24 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 // import sequelize connection
 
-// const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
-// const sess = 
-// {
-//     secret: 'Super secret secret',
-//     cookies: {},
-//     resave: false,
-//     saveUninitialized: true,
-//     store: new SequelizeStore(
-//     {
-//         db: sequelize
-//     })
-// }
+const sess = 
+{
+    secret: 'Super secret secret',
+    cookies: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore(
+    {
+        db: sequelize
+    })
+}
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// app.use(session(sess));
+app.use(session(sess));
 //Sets our app to use the handlebars engine
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
@@ -40,11 +40,11 @@ res.render('main', {layout: 'index'});
 });
 
 // sync sequelize models to the database, then turn on the server
-// sequelize.sync({ force: false })
-// .then(() =>
-// {
+sequelize.sync({ force: false })
+.then(() =>
+{
     app.listen(PORT, () =>
     {
         console.log(`App listening to port ${PORT}`);
     });
-// });
+});
