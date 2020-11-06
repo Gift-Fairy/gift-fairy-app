aec6d8f4b0038d6cafe7791eeee6e659d7f4bcd2
Monica <monitechi8@gmail.com>
2020-11-05 18:05:20 -0600

    adj basic handlebar syntax

--- server.js
+++ server.js
@@ -9,16 +9,14 @@ const handlebars = require('express-handlebars');
 app.set('view engine', 'handlebars');
 //Sets handlebars configurations (we will go through them later on)
 app.engine('handlebars', handlebars({
-layoutsDir: __dirname + '/views/layouts',
+layoutsDir: `${__dirname}/views/layouts`
 }));
-
-
 
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
 //Serves the body of the page aka "main.handlebars" to the container //aka "index.handlebars"
-res.render('main', {layout : 'index'});
+res.render('main', {layout: 'index'});
 });
 
 app.listen(port, () => console.log(`App listening to port ${port}`));
