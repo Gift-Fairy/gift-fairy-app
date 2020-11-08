f476ef47d89bdc175fd08502bc5b47e6f962205e
Devin Gibson <dgib21@hotmail.com>
2020-11-08 14:24:11 -0600

    posting a new user route

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -13,5 +13,31 @@ router.get('/', (req, res) => {
         res.status(500).json(err);
       });
   });
+  
+// post a new user
+  router.post('/', (req, res) => {
+    // expects 
+    User.create({
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      email: req.body.email,
+      password: req.body.password,
+      dob: req.body.dob
+    })
+      .then(dbUserData => {
+        req.session.save(() => {
+          req.session.user_id = dbUserData.id;
+          req.session.email = dbUserData.email;
+          req.session.loggedIn = true;
+    
+          res.json(dbUserData);
+        });
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
+
 
   module.exports = router;
