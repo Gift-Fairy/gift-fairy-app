672266bb927e57144c1fe4f30dcc8993c784f139
Devin Gibson <dgib21@hotmail.com>
2020-11-08 08:08:43 -0600

    fixed get all users route

--- controllers/index.js
+++ controllers/index.js
@@ -2,9 +2,12 @@ const router = require('express').Router();
 
 const homeRoutes = require('./home-routes.js');
 const formRoutes = require('./form-routes.js');
+const apiRoutes = require('./api/');
+
 
 router.use('/', homeRoutes);
 router.use('/form', formRoutes);
+router.use('/api', apiRoutes);
 
 router.use((req, res) =>
 {
