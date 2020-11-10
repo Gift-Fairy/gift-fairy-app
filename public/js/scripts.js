4027d7545d68ea359374f58bc682065b4ad9ecb5
Ida <67982739+igk1024@users.noreply.github.com>
2020-11-10 17:25:54 -0600

    added birthdate functionality

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -25,3 +25,24 @@ if (element && element.bulmaCarousel) {
 		console.log(state);
 	});
 }
+
+//bulma calendar
+// Initialize all input of type date
+var calendars = bulmaCalendar.attach('[type="date"]', options);
+
+// Loop on each calendar initialized
+for(var i = 0; i < calendars.length; i++) {
+	// Add listener to date:selected event
+	calendars[i].on('select', date => {
+		console.log(date);
+	});
+}
+
+// To access to bulmaCalendar instance of an element
+var element = document.querySelector('#birthday');
+if (element) {
+	// bulmaCalendar instance is available as element.bulmaCalendar
+	element.bulmaCalendar.on('select', function(datepicker) {
+		console.log(datepicker.data.value());
+	});
+}
