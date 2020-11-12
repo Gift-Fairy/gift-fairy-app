1521d4b3ee8b80910b9de5178df0c0b6a7216562
Monica <monitechi8@gmail.com>
2020-11-12 12:49:29 -0600

    modal function and remove category function

--- public/js/form.js
+++ public/js/form.js
@@ -1,50 +1,22 @@
-
 $(document).ready(function () {
-    // hidden elements
     $('#form').show();
-    $('#wishlist-container').hide();
-    $('#testemail').hide();
 
     $('#register').on('click', function (event) {
-    
-    $('#form').hide();
-    $('#wishlist-container').show();
-    $('#testemail').hide();
+        $('#wishlist-container').show();
 
-
-
-        // category input
-        $('#cat-btn').on('click', function (event) {
-            event.preventDefault();
-
-            var cat0 = $('#cat0').val().trim();
-            var cat1 = $('#cat1').val().trim();
-            var cat2 = $('#cat2').val().trim();
-
-            console.log(cat0, cat1, cat2);
-
-            if (cat0 != null) { $("#catorigin0").val('').replaceWith(cat0) } else { $("#origin0").hide() };
-            if (cat1 != null) { $("#catorigin1").val('').replaceWith(cat1) } else { $("#origin1").hide() };
-            if (cat2 != null) { $("#catorigin2").val('').replaceWith(cat2) } else { $("#origin2").hide() };
-
-            createWishlist();
-
-        });
     });
 
-});
+    $('#user-info').click(function () {
 
-function createWishlist() {    // get input from personal preferences section
-    $('#details-info').click(function () {
-
+        // input in email inbox
         var user = {};
         user.shoe = $('').val().trim();
         user.pant = $('').val().trim();
         user.shirt = $('').val().trim();
         user.brands = $('').val().trim();
 
+        // adding user details to the wishlist
         $(function () {
-
 
         });
     });
@@ -60,7 +32,6 @@ function createWishlist() {    // get input from personal preferences section
         console.log(brand, product, categoryInput);
 
         // add items to the wishlist
-
         $("<li>").html(brand + ' ' + product).addClass("panel-block").appendTo("#items");
 
     });
@@ -71,26 +42,15 @@ function createWishlist() {    // get input from personal preferences section
 
         $("<li>").html("");
 
-        function toggleModalClasses(event) {
-            var modalId = event.currentTarget.dataset.modalId;
-            var modal = $(modalId);
-            modal.toggleClass('is-active');
-            $('html').toggleClass('is-clipped');
-        };
-
-        $('.save-wishlist').click(toggleModalClasses);
-        $('.close-modal').click(toggleModalClasses);
-
     });
 
-    // // delete items
-    // let item = document.querySelector('.panel-block');
+        // Share wishlist email modal
+        $("#share-btn").click(function () {
+            $(".modal").addClass("is-active");
+        });
 
-    // item.addEventListener('click', () => {
-    //     (item.querySelectorAll('.notification .delete') || []).forEach(($delete) => {
-    //       var $notification = $delete.parentNode;
+        $(".modal-close").click(function () {
+            $(".modal").removeClass("is-active");
+        });
 
-    //       $delete.addEventListener('click', () => {
-    //         $notification.parentNode.removeChild($notification);
-    //       });
-    //     });
+    });
