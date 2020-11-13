297305916f41fbe1896f5da0bc053c48f14baded
Monica <monitechi8@gmail.com>
2020-11-13 16:26:24 -0600

    fixed add + remove items

--- public/js/wishlist.js
+++ public/js/wishlist.js
@@ -1,21 +1,10 @@
-$('#user-info').click(function () {
-
-    // input in email inbox
-    var user = {};
-    user.shoe = $('').val().trim();
-    user.pant = $('').val().trim();
-    user.shirt = $('').val().trim();
-    user.brands = $('').val().trim();
-
-    // adding user details to the wishlist
-    $(function () {
-
-    });
+$(document).ready(function () {
 });
 
+// delete function
 $('#fashion, #electronics, #home, #all, #pets').on('click', '.delete', function () {
-    $(this).parent('p').remove();
-  });
+  $(this).parent('p').remove();
+});
 
 // add wishitem
 $("#add-item").on('click', function (event) {
@@ -27,28 +16,27 @@ $("#add-item").on('click', function (event) {
 
   console.log(brand, product, categoryId);
 
-  // add items to the wishli
+  // add items to the wishlist
   $("<p>").html(brand + ' ' + product + '<button class="delete is-pulled-left"></button>').appendTo("#all");
 
 });
 
-
 // Share wishlist email modal
 $("#share-btn").click(function () {
-    $(".modal").addClass("is-active");
+  $(".modal").addClass("is-active");
 });
 
 $(".modal-close").click(function () {
-    $(".modal").removeClass("is-active");
+  $(".modal").removeClass("is-active");
 });
 
 // wishlist panel toggle
 $(function () {
-    $('ul.li.tablinks:first').addClass('is-active');
-    $('li.tablinks').click(function () {
-        $('li.tablinks').removeClass('is-active');
-        $(this).addClass('is-active');
-        return true;
-    });
-    $('.tabcontent:first').show();
-});
+  $('ul.li.tablinks:first').addClass('is-active');
+  $('li.tablinks').click(function () {
+    $('li.tablinks').removeClass('is-active');
+    $(this).addClass('is-active');
+    return true;
+  });
+  $('.tabcontent:first').show();
+});  
