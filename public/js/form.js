20ddfafd93442dfb605c8b7e86e2808d5fae91b1
Monica <monitechi8@gmail.com>
2020-11-13 02:59:58 -0600

    update  form.js

--- public/js/form.js
+++ public/js/form.js
@@ -1,56 +1,8 @@
-$(document).ready(function () {
-    $('#form').show();
-
-    $('#register').on('click', function (event) {
-        $('#wishlist-container').show();
-
+$('#register').on('click', function (event) {
+    
     });
 
-    $('#user-info').click(function () {
-
-        // input in email inbox
-        var user = {};
-        user.shoe = $('').val().trim();
-        user.pant = $('').val().trim();
-        user.shirt = $('').val().trim();
-        user.brands = $('').val().trim();
-
-        // adding user details to the wishlist
-        $(function () {
-
-        });
-    });
-
-    // Add item button
-    $("#add-item").on('click', function (event) {
-        event.preventDefault();
-
-        var categoryInput = $('#category').val().trim();
-        var brand = $('#brand').val().trim();
-        var product = $('#product').val().trim();
-
-        console.log(brand, product, categoryInput);
-
-        // add items to the wishlist
-        $("<li>").html(brand + ' ' + product).addClass("panel-block").appendTo("#items");
-
-    });
-
-    // Delete item button
-    $("#delete").on('click', function (event) {
-        event.preventDefault();
-
-        $("<li>").html("");
-
-    });
-
-        // Share wishlist email modal
-        $("#share-btn").click(function () {
-            $(".modal").addClass("is-active");
-        });
-
-        $(".modal-close").click(function () {
-            $(".modal").removeClass("is-active");
-        });
-
+    
+$('#register').on('click', function (event) {
+    
     });
