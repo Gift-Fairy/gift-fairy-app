c8662030b7d6f57697d6b527356c6b99a6ba8acd
Devin Gibson <dgib21@hotmail.com>
2020-11-12 22:04:51 -0600

    login work

---
+++ public/js/login.js
@@ -0,0 +1,29 @@
+async function loginFormHandler(event) {
+    event.preventDefault();
+  
+    const email = document.querySelector('#email-address').value.trim();
+    const password = document.querySelector('#password-login').value.trim();
+  
+    if (email && password) {
+      const response = await fetch('/api/users/login', {
+        method: 'post',
+        body: JSON.stringify({
+          email,
+          password
+        }),
+        headers: { 'Content-Type': 'application/json' }
+      });
+  
+      if (response.ok) {
+        document.location.replace('/list/');
+      } else {
+        alert(response.statusText);
+      }
+    }
+  }
+  
+
+  
+  document.querySelector('.register').addEventListener('submit', loginFormHandler);
+  
+  
